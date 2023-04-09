ba14d4bb6d874c11ac45fc8903248ce61d7d1d6d
carlosgovi <cgovi66@gmail.com>
2023-04-09 18:14:58 -0300

    change in auth index middleware

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -3,9 +3,12 @@ import { firestore } from "lib/firebase";
 import { Auth } from "models/auth";
 import { User } from "models/user";
 import { sendCode } from "controller/auth";
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+import { handlerCORS } from "lib/middelwares";
+
+async function handlerAuth(req: NextApiRequest, res: NextApiResponse) {
   console.log("aqui el req.body", req.body);
 
   const result = await sendCode(req.body.email);
   return res.send(result);
 }
+export default handlerCORS(handlerAuth);
