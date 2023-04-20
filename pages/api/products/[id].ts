07d7596c21a94ce91168a3859235a85a91949059
carlosgovi <cgovi66@gmail.com>
2023-04-20 18:09:40 -0300

    fixed endpoind products for objectID

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -6,17 +6,29 @@ import { handlerCORS } from "lib/middelwares";
 // ejemplo de la request (((http://localhost:3000/api/products/attw1WlLKTAd2HZLo)))
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const query = req.query.id as string;
-
+  const objectID = req.query.id as string;
   const querySearch = req.query.search as string;
 
-  const searchResults = await productIndex.search(query).then(({ hits }) => {
-    res.send({
-      results: hits,
-      longitud: { total: hits.length },
+  const searchResults = await productIndex
+    .getObject(objectID)
+    .then((result) => {
+      res.send({
+        result: result,
+      });
     });
-  });
 }
 
 const authMiddlewarePass = authMiddleware(handler);
 export default handlerCORS(authMiddlewarePass);
+// async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const query = req.query.id as string;
+
+//   const querySearch = req.query.search as string;
+
+//   const searchResults = await productIndex.search(query).then(({ hits }) => {
+//     res.send({
+//       results: hits,
+//       longitud: { total: hits.length },
+//     });
+//   });
+// }
